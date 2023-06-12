90482cdfb09cf6e04e4bdfe6c851c163e8456a94
Tebogo <herroldmoremi@gmail.com>
2023-06-12 19:41:17 +0200

    basics of javascripts

--- lesson4.js
+++ lesson4.js
@@ -1,22 +1,46 @@
-const object={
-    a:1,b:2,c:3
-}
-//for in use most when itereating objects
-for(const item in object){
-    console.log(`${item} -- ${object[item]}`);
-}
-// for of used mostly on arrays
-const arra=[1,3,5,6]
+// const object={
+//     a:1,b:2,c:3
+// }
+// //for in use most when itereating objects
+// for(const item in object){
+//     console.log(`${item} -- ${object[item]}`);
+// }
+// // for of used mostly on arrays
+// const arra=[1,3,5,6]
 
-for(const item of arra){
-    console.log(`${item} `)
-}
+// for(const item of arra){
+//     console.log(`${item} `)
+// }
 
-let obj={
-    name:"Tebogo",
-    age:30,
-    home:"Morapalala"
-}
-for(const item in obj){
-    console.log(`${item}----${obj[item]}`)
+// let obj={
+//     name:"Tebogo",
+//     age:30,
+//     home:"Morapalala"
+// }
+// for(const item in obj){
+//     console.log(`${item}----${obj[item]}`)
+// }
+// Setup
+const myArray = [];
+
+// Only change code below this line
+// for(let i=1;i<10;i++){
+//   if(i%2!=0){
+//     myArray.push(i);
+//   }
+//   else{
+//     continue;
+//   }
+// }
+// console.log(myArray)
+// Setup
+const myArr = [2, 3, 4, 5, 6];
+
+// Only change code below this line
+
+// for(let i of myArr){
+//     console.log(i)
+// }
+for(let i=0;i<myArr.length;i++){
+    console.log(myArr[i]);
 }
