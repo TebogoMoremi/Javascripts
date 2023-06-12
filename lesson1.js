8d6b264f4c286c70abe770e0afc334df53dd1fe3
Tebogo <herroldmoremi@gmail.com>
2023-06-12 12:27:19 +0200

    basics

--- lesson1.js
+++ lesson1.js
@@ -1,1 +1,14 @@
-console.log("Hello javascripts");
+//switch case
+
+const fruit="strawberry"
+
+switch(fruit){
+    case "strawberry":
+    case "Rasberry":
+        console.log("A "+fruit+" is red");
+        break
+    case "blueberry":
+        console.log("B "+fruit+"is blur")
+    default:
+        console.log("I dont know!")
+}
